1da107026889cac74d6167f17dd90a0c48b23d3f
Zooltan <r.ovlesen@gmail.com>
2023-07-04 19:37:21 +0200

    Don't cast debuff on target that already has it.

--- src/model/simulation.ts
+++ src/model/simulation.ts
@@ -202,8 +202,13 @@ function getNextTarget(combattant: Combattant, action: FinalAction, allies: Comb
         })
     }
 
+    let filteredEnemies = enemies
+    if (action.type === "debuff") {
+        filteredEnemies = enemies.filter(a1 => !a1.finalState.buffs.get(action.id))
+    }
+
     if (action.target.startsWith('ally') && (allies.length === 0)) return undefined
-    if (action.target.startsWith('enemy') && (enemies.length === 0)) return undefined
+    if (action.target.startsWith('enemy') && (filteredEnemies.length === 0)) return undefined
 
     if (action.target === 'self') return combattant
     if (action.target === "ally with the highest AC") return allies.reduce((a1, a2) => (a1.creature.AC > a2.creature.AC) ? a1 : a2)
@@ -211,11 +216,11 @@ function getNextTarget(combattant: Combattant, action: FinalAction, allies: Comb
     if (action.target === "ally with the most HP") return allies.reduce((a1, a2) => (a1.finalState.currentHP > a2.finalState.currentHP) ? a1 : a2)
     if (action.target === "ally with the least HP") return allies.reduce((a1, a2) => (a1.finalState.currentHP < a2.finalState.currentHP) ? a1 : a2)
     if (action.target === "ally with the highest DPR") return getHighestDPR(allies)
-    if (action.target === "enemy with highest AC") return enemies.reduce((a1, a2) => (a1.creature.AC > a2.creature.AC) ? a1 : a2)
-    if (action.target === "enemy with lowest AC") return enemies.reduce((a1, a2) => (a1.creature.AC < a2.creature.AC) ? a1 : a2)
-    if (action.target === "enemy with most HP") return enemies.reduce((a1, a2) => (a1.finalState.currentHP + (a1.finalState.tempHP || 0) > a2.finalState.currentHP + (a2.finalState.tempHP || 0)) ? a1 : a2)
-    if (action.target === "enemy with least HP") return enemies.reduce((a1, a2) => (a1.finalState.currentHP + (a1.finalState.tempHP || 0) < a2.finalState.currentHP + (a2.finalState.tempHP || 0)) ? a1 : a2)
-    /* if (action.target === "enemy with highest DPR") */ return getHighestDPR(enemies)
+    if (action.target === "enemy with highest AC") return filteredEnemies.reduce((a1, a2) => (a1.creature.AC > a2.creature.AC) ? a1 : a2)
+    if (action.target === "enemy with lowest AC") return filteredEnemies.reduce((a1, a2) => (a1.creature.AC < a2.creature.AC) ? a1 : a2)
+    if (action.target === "enemy with most HP") return filteredEnemies.reduce((a1, a2) => (a1.finalState.currentHP + (a1.finalState.tempHP || 0) > a2.finalState.currentHP + (a2.finalState.tempHP || 0)) ? a1 : a2)
+    if (action.target === "enemy with least HP") return filteredEnemies.reduce((a1, a2) => (a1.finalState.currentHP + (a1.finalState.tempHP || 0) < a2.finalState.currentHP + (a2.finalState.tempHP || 0)) ? a1 : a2)
+    /* if (action.target === "enemy with highest DPR") */ return getHighestDPR(filteredEnemies)
 }
 
 // Calculates which actions each creature is going to take, and saves them in combattant.actions
